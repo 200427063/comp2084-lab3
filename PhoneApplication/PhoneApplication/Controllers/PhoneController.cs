56d6ce5ac1aada324decec709c3624e8513ff953
krens patel <krenspatel17@gmail.com>
2020-01-23 22:03:21 -0500

    LAB 3

---
+++ PhoneApplication/PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            var phoneList = new List<Phone>
+            {
+                new Phone { PhoneName = "Samsung"},
+                new Phone { PhoneName = "Apple"},
+                new Phone { PhoneName = "Blackberry"},
+                new Phone { PhoneName = "OnePlus"},
+                new Phone { PhoneName = "Google"}
+
+            };
+            return View(phoneList);
+        }
+    }
+}
---
+++ PhoneApplication/PhoneApplication/Models/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneId = 0;
+            PhoneName = "";
+            Manufacture = "";
+            MSRP = 0;
+            ScreenSize = 0;
+            Date = DateTime.Parse("01/01/2000") ;
+        }
+        public int PhoneId { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacture { get; set; }
+        public double MSRP { get; set; }
+        public double ScreenSize { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
